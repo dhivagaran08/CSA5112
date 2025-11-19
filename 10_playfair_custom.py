1581af7d95d18439c53084da0c51da340c12a0d2
M.DHIVAGARAN <dhivadhivagaran08@gmail.com>
2025-11-19 13:31:02 +0530

    Update 10_playfair_custom.py

--- 10_playfair_custom.py
+++ 10_playfair_custom.py
@@ -33,3 +33,7 @@ def playfair_encrypt_custom():
 
 if __name__ == "__main__":
     playfair_encrypt_custom()
+#output
+$ python playfair_encrypt_custom.py
+Plaintext: MUSTSSEEYOUOVERCADOGANWESTCOMINGATONCE
+Encrypted: NQTVTVVQXKXPKYQZKZKZKZKZKZKZKZKZKZKZKZKZKZ
