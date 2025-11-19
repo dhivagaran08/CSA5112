f6c1028df72506048a7c77643233019a1928a1fa
M.DHIVAGARAN <dhivadhivagaran08@gmail.com>
2025-11-19 14:25:58 +0530

    Update 27_rsa_small_message.py

--- 27_rsa_small_message.py
+++ 27_rsa_small_message.py
@@ -1,23 +1,52 @@
-def integer_root(n, e):
-    return int(n ** (1 / e))
+import string
 
-def rsa_small_message_attack(c, e):
-    return integer_root(c, e)
+A = string.ascii_uppercase
+def L2I(c): return A.index(c)
+def I2L(i): return A[i]
 
-e = 3
-p = 61
-q = 53
-n = p * q
-m = 5
-c = pow(m, e, n)
+def gen():
+    p=10007; q=10009
+    n=p*q; phi=(p-1)*(q-1)
+    e=65537
+    if phi%e==0: e=17
+    d=pow(e,-1,phi)
+    return e,d,n
 
-recovered = rsa_small_message_attack(c, e)
+def enc(msg,e,n):
+    out=[]
+    for ch in msg.upper():
+        if ch in A:
+            m=L2I(ch)
+            out.append(pow(m,e,n))
+        else:
+            out.append(None)
+    return out
 
-print("Original Message :", m)
-print("Ciphertext       :", c)
-print("Recovered Message:", recovered)
+def dec(c,d,n):
+    out=[]
+    for x in c:
+        if x is None: out.append('?')
+        else: out.append(I2L(pow(x,d,n)))
+    return ''.join(out)
+
+def attack(c,e,n):
+    table={}
+    for m in range(26):
+        table[pow(m,e,n)] = m
+    out=[]
+    for x in c:
+        if x is None: out.append('?')
+        else: out.append(I2L(table[x]))
+    return ''.join(out)
+
+e,d,n = gen()
+msg = "HELLO WORLD"
+c = enc(msg,e,n)
+print("Cipher:", c)
+print("Bob decrypts:", dec(c,d,n))
+print("Attacker recovers:", attack(c,e,n))
 #output
-Original Message : 5
-Ciphertext       : 125
-Recovered Message: 5
+Cipher: [50837557, 49384228, 37220916, 37220916, 79978083, None, 17548935, 79978083, 37220916, 14904002, 49384228]
+Bob decrypts: HELLO?WORLD
+Attacker recovers: HELLO?WORLD
 
